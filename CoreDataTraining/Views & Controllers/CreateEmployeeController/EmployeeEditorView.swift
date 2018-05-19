f39d3564c3c63a39d4e5f460f425fa81da6124f0
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-05-19 20:40:14 +1000

    Custom views
    custom views added
    constraints done

---
+++ CoreDataTraining/Views & Controllers/CreateEmployeeController/EmployeeEditorView.swift
@@ -0,0 +1,21 @@
+//
+//  EmployeeEditorView.swift
+//  CoreDataTraining
+//
+//  Created by Siyu Liu on 19/5/18.
+//  Copyright © 2018 Siyu Liu. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeEditorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
